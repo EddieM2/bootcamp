a4a6baf466671f24ba8b1c64157198ff82e7c190
unknown <ed.chamal@duocuc.cl>
2023-06-12 22:11:40 -0400

    bases

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    <title>Document</title>
+</head>
+<body>
+    <header class="navegacion-principal">
+        <div class="contenedor contenido-header">
+            <h1>Titulo</h1>
+            <nav class="navegacion-principal">
+                <a href="">Link 1</a>
+                <a href="">Link 2</a>
+                <a href="">Link 3</a>
+            </nav>
+    
+        </div>
+    </header>
+</body>
+</html>
